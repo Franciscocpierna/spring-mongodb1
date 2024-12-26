ccd2dd13c747cdfd6b4e0a9211d40ce2f1a58656
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-12-26 14:03:46 -0300

    Projeção dos dados do autor com DTO 26/12/2024

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -1,23 +1,30 @@
-package com.nelioalves.workshopmongo.domain;
 
+package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.nelioalves.workshopmongo.dto.AuthorDTO;
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
-	
+	//private User author;
+	private AuthorDTO author;
+
 	public Post() {
 	}
-	public Post(String id, Date date, String title, String body, User author) {
+
+	
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -25,36 +32,49 @@ public class Post implements Serializable {
 		this.body = body;
 		this.author = author;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getBody() {
 		return body;
 	}
+
 	public void setBody(String body) {
 		this.body = body;
 	}
-	public User getAuthor() {
+
+	
+	public AuthorDTO getAuthor() {
 		return author;
 	}
-	public void setAuthor(User author) {
+
+	
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -62,6 +82,7 @@ public class Post implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
