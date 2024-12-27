ca8efa94b1006cedcb47bda2d5a2bf0d6db6dd4b
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-12-27 07:05:57 -0300

    Consulta simples com @Query 27/12/2024

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -2,8 +2,11 @@ package com.nelioalves.workshopmongo.repository;
 
 
 
+
 import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.workshopmongo.domain.Post;
@@ -11,5 +14,8 @@ import com.nelioalves.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
